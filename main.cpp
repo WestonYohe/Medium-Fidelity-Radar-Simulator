1e97ababa1f57eb2844c10ae499ba89388715764
WestonYohe <westonyohe@gmail.com>
2022-08-11 12:16:53 -0500

    Fixed input file location string in main.cpp

--- main.cpp
+++ main.cpp
@@ -22,16 +22,16 @@ using namespace std;
 int main() {
    srand(time(0));
    configManager test1;
-    test1.inputRadarFile("RadarInfo.txt");
-    test1.inputFaceFile("FaceInfo.txt");
-    test1.inputSectorFile("SectorInfo.txt");
-    test1.inputTargetFile("TargetInfo.txt");
+    test1.inputRadarFile("inputFiles/RadarInfo.txt");
+    test1.inputFaceFile("inputFiles/FaceInfo.txt");
+    test1.inputSectorFile("inputFiles/SectorInfo.txt");
+    test1.inputTargetFile("inputFiles/TargetInfo.txt");
 
     
    radar simRadar;
     test1.initializeInputData(simRadar);
-    //simRadar.printSimInfo(simRadar);
-    simRadar.startSimulation();
+    simRadar.printSimInfo(simRadar);
+    //simRadar.startSimulation();
     //test1.searchDataOutput(simRadar,"searchData.txt");
     test1.closeInputFiles();
     
